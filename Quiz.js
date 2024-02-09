f4d05f3857e14d60712468c6710cdb9a7ceaaee7
Patopam <145809595+Patopam@users.noreply.github.com>
2024-02-09 17:50:50 -0500

    Add files via upload

---
+++ Quiz.js
@@ -0,0 +1,8 @@
+const GradesEstudents = [100, 30, 40, 80, 10];
+const Results = computedGrades(GradesEstudents);
+console.log(Results);
+
+var numbers = [100, 30, 40, 80, 10];
+var doubles = numbers.map(function (x) {
+	return;
+});
